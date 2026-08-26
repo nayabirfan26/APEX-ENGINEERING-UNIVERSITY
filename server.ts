f4593261f236f8fcc6f96687da0709191910e6ef
NAYAB KHAN <nayabirfan26@gmail.com>
2026-08-26 20:55:11 +0500

    Use Render port

--- server.ts
+++ server.ts
@@ -6,7 +6,7 @@ import { GoogleGenAI } from "@google/genai";
 import { connectDB } from "./server/db";
 
 const app = express();
-const PORT = 3000;
+const PORT = Number(process.env.PORT) || 3000;
 
 app.use(express.json());
 
